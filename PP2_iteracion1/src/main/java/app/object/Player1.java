2064b2bb6b533d0a812140ae7ecae3157866e264
Lautaro <lautydb@yahoo.com>
2017-05-21 15:18:42 -0300

    PLayers

---
+++ PP2_iteracion1/src/main/java/app/object/Player1.java
@@ -0,0 +1,17 @@
+package app.object;
+
+public class Player1 {
+	private Puntaje puntaje;
+	
+	public Player1(Puntaje puntaje){
+		this.puntaje = puntaje;
+	}
+
+	public Puntaje getPuntaje() {
+		return puntaje;
+	}
+
+	public void setPuntaje(Puntaje puntaje) {
+		this.puntaje = puntaje;
+	}
+}
