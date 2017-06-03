2f434cf7e47491e57be5f749610c128eccb73b99
lautaro <lautaro@lautaro-PCW20>
2017-06-03 19:37:30 -0300

    mapa

--- PP2_iteracion1/src/main/java/app/mapa/MapaTiled.java
+++ PP2_iteracion1/src/main/java/app/mapa/MapaTiled.java
@@ -6,13 +6,14 @@ import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import app.object.Coordinate;
+import app.object.Size;
 import app.util.CargadorRecursos;
 
 public class MapaTiled {
 	private int anchoMapaEnTiles;
 	private int altoMapaEnTiles;
-	private Coordinate coordinatePlayer1;
-	private Coordinate coordinatePlayer2;
+//	private Coordinate coordinatePlayer1;
+//	private Coordinate coordinatePlayer2;
 	
 	private ArrayList<CapaSprites> capasDeSprites;
 	private ArrayList<CapaColisiones> capasDeColisiones;
@@ -24,20 +25,43 @@ public class MapaTiled {
 		anchoMapaEnTiles = obtenerIntDesdeJSON(globalJSON,"width");
 		altoMapaEnTiles = obtenerIntDesdeJSON(globalJSON,"height");
 
-		//players
-		JSONObject player1 = obtenerObjetoJSON(globalJSON.get("player1").toString());
-		coordinatePlayer1 = new Coordinate(obtenerIntDesdeJSON(player1, "x"), obtenerIntDesdeJSON(player1, "y"));
-		JSONObject player2 = obtenerObjetoJSON(globalJSON.get("player2").toString());
-		coordinatePlayer2 = new Coordinate(obtenerIntDesdeJSON(player2, "x"), obtenerIntDesdeJSON(player2, "y"));
-		
+//		//players
+//		JSONObject player1 = obtenerObjetoJSON(globalJSON.get("player1").toString());
+//		coordinatePlayer1 = new Coordinate(obtenerIntDesdeJSON(player1, "x"), obtenerIntDesdeJSON(player1, "y"));
+//		JSONObject player2 = obtenerObjetoJSON(globalJSON.get("player2").toString());
+//		coordinatePlayer2 = new Coordinate(obtenerIntDesdeJSON(player2, "x"), obtenerIntDesdeJSON(player2, "y"));
+//		
 		//CAPAS
 		JSONArray capas = obtenerArrayJSON(globalJSON.get("layers").toString());
 		this.capasDeSprites = new ArrayList<CapaSprites>();
 		this.capasDeColisiones = new ArrayList<CapaColisiones>();
 		
 		//INICIAR CAPAS
+		//RECORRE TODAS LAS CAPAS
 		for(int i=0; i < capas.size(); i++){
+			JSONObject datosCapa = obtenerObjetoJSON(capas.get(i).toString());
 			
+			int anchoCapa = obtenerIntDesdeJSON(datosCapa, "width");
+			int altoCapa = obtenerIntDesdeJSON(datosCapa, "height");
+			int xCapa = obtenerIntDesdeJSON(datosCapa, "y");
+			int yCapa = obtenerIntDesdeJSON(datosCapa, "x");
+			
+			String tipo = datosCapa.get("type").toString();
+			
+			switch(tipo){
+				case "tilelayer":
+					JSONArray sprites = obtenerArrayJSON(datosCapa.get("data").toString());
+					int[] spritesCapa = new int [sprites.size()];
+					for(int j=0; j < sprites.size();j++){
+						//aca tenemos cada numero del vector de una capa
+						int codigoSprite = Integer.parseInt(sprites.get(j).toString());
+						spritesCapa[j] = codigoSprite -1;
+					}
+					this.capasDeSprites.add(new CapaSprites(new Size (anchoCapa,altoCapa),new Coordinate (xCapa,yCapa),spritesCapa));
+					break;
+				case "objectgroup":
+					break;
+			}
 		}
 	}
 	
