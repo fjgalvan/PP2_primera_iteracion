9718587d9223f3c75493cf16a43f4973266c87c5
fedebarrios <fede.barrios@hotmail.com>
2017-06-07 15:10:42 -0300

    Se elimino la clase Puntaje
    Tambien se adaptaron las clases que la implementaban.

--- PP2_iteracion1/src/main/java/app/object/Player1.java
+++ PP2_iteracion1/src/main/java/app/object/Player1.java
@@ -1,21 +1,30 @@
 package app.object;
 
 public class Player1 {
-	private Puntaje puntaje;
+	private double puntaje;
+	private int cantidadDeEnemigosAsesinados;
 	private KeyEventListener listener;
-	
-	public Player1(Puntaje puntaje,Draftsman dibujador){
+
+	public Player1(double puntaje, int cantidadDeEnemigosAsesinados, Draftsman dibujador) {
 		this.puntaje = puntaje;
+		this.cantidadDeEnemigosAsesinados = cantidadDeEnemigosAsesinados;
 		this.listener = new KeyEventListener(dibujador.getEntorno());
 	}
 
-	public Puntaje getPuntaje() {
-		return puntaje;
+	public double getPuntaje() {
+		return this.puntaje;
 	}
 
-	public void setPuntaje(Puntaje puntaje) {
-		this.puntaje.setPuntaje(this.puntaje.getPuntaje() + puntaje.getPuntaje());
-		this.puntaje.setCantidadDeEnemigosAsesinados(this.puntaje.getCantidadDeEnemigosAsesinados() + puntaje.getCantidadDeEnemigosAsesinados());
+	public int getCantidadDeEnemigosAsesinados() {
+		return this.cantidadDeEnemigosAsesinados;
+	}
+
+	public void setPuntaje(double puntaje) {
+		this.puntaje += puntaje;
+	}
+
+	public void setCantidadDeEnemigosAsesinados(int cantidadDeEnemigosAsesinados) {
+		this.cantidadDeEnemigosAsesinados += cantidadDeEnemigosAsesinados;
 	}
 
 	public KeyEventListener getListener() {
