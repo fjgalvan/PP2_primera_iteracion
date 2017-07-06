5df442cfb086adf8fd0f4bba833a9e5572d61002
fjgalvan <fjgalvanx87@gmail.com>
2017-07-06 14:24:02 -0300

    error test de mapasTiled ???

--- PP2_iteracion1/src/test/java/app/mapa/TestMapaTiled.java
+++ PP2_iteracion1/src/test/java/app/mapa/TestMapaTiled.java
@@ -6,42 +6,42 @@ import org.junit.Test;
 import app.object.ListStructures;
 
 public class TestMapaTiled {
-	/*@Test
+	@Test
 	public void testIniciar() {
 		//String mapaAJugar ="/mapas/MapaTank03.csv";
 		MapaTiled mapa = new MapaTiled("/mapas/MapaTank03.csv");
 		mapa.inicializar();
 		assertNotNull(mapa);
-	}*/
+	}
 	
-	/*@Test
+	@Test
 	public void testObtenerCapas(){
 		MapaTiled mapa = new MapaTiled("/mapas/MapaTank03.csv");
 		mapa.inicializar();
 		mapa.obtenerCapas();
 		assertNotNull(mapa.getCapasDeSprites().size());
-	}*/
+	}
 
-	/*@Test
+	@Test
 	public void testTraerImagenDesdeJSON(){
 		MapaTiled mapa = new MapaTiled("/mapas/MapaTank03.csv");
 		mapa.inicializar();
 		assertNotNull(mapa.traerImagenDesdeJSON(1));
-	}*/
+	}
 	
-	/*@Test
+	@Test
 	public void testObtenerCoordenada(){
 		MapaTiled mapa = new MapaTiled("/mapas/MapaTank03.csv");
 		mapa.inicializar();
 		assertTrue(mapa.obtenerCoordenada(40, 15, 25, 5).equals(new Point(200,0)));
-	}*/
+	}
 	
-	/*@Test
+	@Test
 	public void testCrearEstructuras(){
 		MapaTiled mapa = new MapaTiled("/mapas/MapaTank03.csv");
 		mapa.inicializar();
 		ListStructures estructuras = new ListStructures();
 		mapa.crearEstructuras(estructuras);
 		assertNotNull(estructuras.getLista().size());
-	}*/
+	}
 }
