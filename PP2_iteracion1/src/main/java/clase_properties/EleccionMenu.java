e0aa58ed0bc2578542b0bf1d5bc868818023f043
fjgalvan <fjgalvanx87@gmail.com>
2017-07-26 00:18:08 -0300

    test properties

--- PP2_iteracion1/src/main/java/clase_properties/EleccionMenu.java
+++ PP2_iteracion1/src/main/java/clase_properties/EleccionMenu.java
@@ -1,34 +1,24 @@
 package clase_properties;
-
 public class EleccionMenu {
 	public String dificultad_elegido;
 	public int enemigos_elegido;
 	public String juego_elegido;
 	public String nivel_elegido;
 	public int vidas_elegido;
-	
-	public EleccionMenu(String dificultad, int enemigos, String juego, String nivel,int vidas)
-	{
+	public EleccionMenu(String dificultad, int enemigos, String juego, String nivel,int vidas){
 		this.dificultad_elegido= dificultad;
 		this.enemigos_elegido= enemigos;
 		this.juego_elegido= juego;
 		this.nivel_elegido= nivel;
-		this.vidas_elegido= vidas;
-	}
-
+		this.vidas_elegido= vidas;}
 	public String getDificultad_elegido() {
 		return dificultad_elegido;}
-
 	public int getEnemigos_elegido() {
 		return enemigos_elegido;}
-
 	public String getJuego_elegido() {
 		return juego_elegido;}
-
 	public String getNivel_elegido() {
 		return nivel_elegido;}
-
 	public int getVidas_elegido() {
 		return vidas_elegido;}
-	
 }
--- PP2_iteracion1/src/main/java/clase_properties/Principal.java
+++ PP2_iteracion1/src/main/java/clase_properties/Principal.java
@@ -1,73 +1,38 @@
 package clase_properties;
-
 import java.io.*;
 import java.util.Properties;
-
 import app.object.Configuracion; 
- 
-/**
- * @author CHENAO
- * 
- */
-public class Principal {
- 
-  /**
-  * Lee un archivo de propiedades desde una ruta especifica
-  * y se imprime en pantalla.
-  */
+public class Principal {//Lee un archivo de propiedades desde una ruta especifica y se imprime en pantalla.
 	public Principal(){}
-	
 	public EleccionMenu leerArchivo() {
-		 try 
-		 {
-			  /**Creamos un Objeto de tipo Properties*/
-			  Properties propiedades = new Properties();
-			  
-			  /**Cargamos el archivo desde la ruta especificada*/
-			  propiedades.load(new FileReader(Configuracion.rutaProperties));//("C:/Users/Usuario/git/PP2_primera_iteracion/PP2_iteracion1/src/main/java/properties/archivo.properties"));
-			  	 
-			  /**Obtenemos los parametros definidos en el archivo*/
-			  String juego = propiedades.getProperty("juego");	  
+		 try {
+			  Properties propiedades = new Properties();/**Creamos un Objeto de tipo Properties*/
+			  propiedades.load(new FileReader(Configuracion.rutaProperties));  /**Cargamos el archivo desde la ruta especificada*/
+			  String juego = propiedades.getProperty("juego");	/**Obtenemos los parametros definidos en el archivo*/  
 		      String dificultad = propiedades.getProperty("dificultad");
 		      int enemigos = Integer.parseInt(propiedades.getProperty("enemigos"));
 		      String nivel = propiedades.getProperty("nivel");
 		      int vidas = Integer.parseInt(propiedades.getProperty("vidas"));
-		      
-		      return new EleccionMenu(dificultad, enemigos, juego, nivel, vidas);
-		      /**Imprimimos los valores*/    
-		  } 
-		  catch (IOException e) 
-		  {
-			  return null;
-		  } 
+		      return new EleccionMenu(dificultad, enemigos, juego, nivel, vidas);   } 
+		  catch (IOException e) {
+			  return null;} 
 	 }
- 
 	 public boolean ModificarArchivo(EleccionMenu eleccion){
 			try {
-			/**Creamos un Objeto de tipo Properties*/
-			  Properties propiedades = new Properties();
-			  
-			  /**Cargamos el archivo desde la ruta especificada*/
-			  
-				propiedades.load(new FileReader("src/main/java/properties/archivo.properties"));//("C:/Users/Usuario/git/PP2_primera_iteracion/PP2_iteracion1/src/main/java/properties/archivo.properties"));
-				
+			  Properties propiedades = new Properties();/**Creamos un Objeto de tipo Properties*/
+				propiedades.load(new FileReader("src/main/java/properties/archivo.properties"));/**Cargamos el archivo desde la ruta especificada*/				
 				propiedades.setProperty("dificultad", eleccion.dificultad_elegido);
 				propiedades.setProperty("enemigos", String.valueOf(eleccion.enemigos_elegido));
 				propiedades.setProperty("juego", eleccion.juego_elegido);
 				propiedades.setProperty("nivel", eleccion.nivel_elegido);
 				propiedades.setProperty("vidas", String.valueOf(eleccion.vidas_elegido));
-				
-				/*propiedades.store(new FileWriter("out.properties"),"nueva eleccion del Menu");*/
-				System.out.println("Se modifico el archivo Properties");
-				
+				System.out.println("Se modifico el archivo Properties");		
 				FileOutputStream file = new FileOutputStream("src/main/java/properties/archivo.properties");//("C:/Users/Usuario/git/PP2_primera_iteracion/PP2_iteracion1/src/main/java/properties/archivo.properties");
 				propiedades.store(file, "Escribiendo");			
-				file.close();
-				
+				file.close();	
 				return true;				
 			} catch (IOException e) {
 				  System.out.println("Error, El archivo no exite o no se puede leer");
-				return false;
-			}
+				return false;}
 	 }
 }
