500475912b13a0057f22bc82dfdac4196f462287
fjgalvan <fjgalvanx87@gmail.com>
2017-07-27 19:07:28 -0300

    test configuracion

--- PP2_iteracion1/src/main/java/app/object/Configuracion.java
+++ PP2_iteracion1/src/main/java/app/object/Configuracion.java
@@ -3,7 +3,9 @@ package app.object;
 import java.util.Random;
 import entorno.Entorno;
 
-public class Configuracion {public static Entorno ENTORNO = null;
+public class Configuracion 
+{
+	public static Entorno ENTORNO = null;
 	
 	public static final double VelocidadTanque = 2;
 	public static final int CantidadMayorLista = 50;
