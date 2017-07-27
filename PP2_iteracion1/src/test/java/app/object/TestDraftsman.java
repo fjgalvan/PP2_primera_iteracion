6bdc8e0ea622818e4d6b0314b653ce69b943bf30
fjgalvan <fjgalvanx87@gmail.com>
2017-07-27 07:51:22 -0300

    testDraftsman

--- PP2_iteracion1/src/test/java/app/object/TestDraftsman.java
+++ PP2_iteracion1/src/test/java/app/object/TestDraftsman.java
@@ -10,24 +10,26 @@ import clase_properties.Principal;
 
 public class TestDraftsman {
 	//ERROR DE TEST CON MAVEN
-	/*@Test
+	@Test
 	public void testDibujarMapa(){
 		Principal principal= new Principal();
-		DataLevelGame data = new ManagerLevel(principal).getDataLevelGame();
+		DataLevelGame data = new ManagerLevelGameMain(principal).getDataLevelGame();
 		Game juegoPrueba= new Game(data);
 		//GraphicMap mapa = new GraphicMap(new Map(new Point(1000, 600)), new Point(20, 20));
 		//Draftsman dibujador = new Draftsman(null, mapa, "Battle-Ungs");
 		Tank tank = new Tank(Orientation.UP,new Point(560,520),new Point(40,40),2);
 		Point coordinate = new Point(30, 50);
 		Point size= new Point(20,20);
+		Bullet bullet= new Bullet(Orientation.UP, coordinate, size);
+		
 		try{
-			juegoPrueba.getMapa();
+			juegoPrueba.getDibujador().dibujarMapa(100, 100, 100, 1, "");
 		} catch(Exception e){
 	        //System.out.println("No se pudo dibujar Mapa!!");
 	    }
 		
 		try{
-			juegoPrueba.getDibujador().dibujarMapa(100, 100, 100, 1, "");
+			juegoPrueba.getDibujador().dibujarEstructura(tank);
 		} catch(Exception e){
 	        //System.out.println("No se pudo dibujar Mapa!!");
 	    }
@@ -38,33 +40,14 @@ public class TestDraftsman {
 	        //System.out.println("No se pudo dibujar Bomberman!!");
 	    }
 		
-		
-		try{
-			juegoPrueba.getDibujador().dibujarImagen("imagen/bala_2.png", coordinate);
-		} catch(Exception e){
-	        //System.out.println("No se pudo dibujar Imagen1!!");
-	    }
-		
-		try{
-			juegoPrueba.getDibujador().dibujarImagen("imagen/bala_2.png", coordinate, 0);
-		} catch(Exception e){
-	        //System.out.println("No se pudo dibujar Imagen2!!");
-	    }
-		
-		try{
-			juegoPrueba.getDibujador().dibujarRectangulo(coordinate, size, 0, null);
-		} catch(Exception e){
-	        //System.out.println("No se pudo dibujar Rectangulo!!");
-	    }
-		
-		try{
-			juegoPrueba.getDibujador().dibujarCirculo(coordinate, 20, null);
-		} catch(Exception e){
-	        //System.out.println("No se pudo dibujar Circulo!!");
-	    }
-		
 		try{
 			juegoPrueba.getDibujador().dibujarTank(tank);
+		} catch(Exception e){
+	        //System.out.println("No se pudo dibujar Tank!");
+	    }
+		
+		try{
+			juegoPrueba.getDibujador().dibujarBullet(bullet);
 		} catch(Exception e){
 	        //System.out.println("No se pudo dibujar Tank!");
 	    }
@@ -98,6 +81,12 @@ public class TestDraftsman {
 		} catch(Exception e){
 	        //System.out.println("No se pudo dibujar EnemyTankIntermediate!!");
 	    }
-	}*/
+		
+		try{
+			juegoPrueba.getDibujador().dibujarFinDeJuego();
+		} catch(Exception e){
+	        //System.out.println("No se pudo dibujar EnemyTankIntermediate!!");
+	    }
+	}
 	
 }
