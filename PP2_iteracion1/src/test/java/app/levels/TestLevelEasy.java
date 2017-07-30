51229c2faaade8d0a4008f194f3a60d30983a595
fjgalvan <fjgalvanx87@gmail.com>
2017-07-30 15:41:15 -0300

    testlevelEasy

--- PP2_iteracion1/src/test/java/app/levels/TestLevelEasy.java
+++ PP2_iteracion1/src/test/java/app/levels/TestLevelEasy.java
@@ -47,4 +47,33 @@ public class TestLevelEasy {
 		easy.finishLevel();
 	}
 	
+	@Test
+	public void testlevelE() 
+	{
+		Tank tank = new Tank(Orientation.LEFT, new Point(300, 200), new Point(40, 40), 2);
+		DataLevelGame data = new DataLevelGame();
+		LevelEasy easy= new LevelEasy(data);
+		easy.containsPlayer1();
+		easy.containsPlayer2();
+		easy.cargarCoordenadasValidas();
+		easy.crearDriverEnemyTank(tank);
+		easy.crearTanquesPlayers();
+		//easy.destruirDriverEnemy(driver);
+		easy.destruirTankEnemy(tank);
+		easy.destruirTankPlayer(tank);
+		easy.destruirTankPlayer1(tank);
+		easy.getDrives();
+		easy.getEnemigos();
+		easy.getEnergyPlayer();
+		easy.getNextCoordinate();
+		easy.getPlayers();
+		easy.getTankController();
+		easy.getTankCopController();
+		easy.getTankPlayer1();
+		easy.getTankPlayer2();
+		//easy.destruirTankPlayer2(tank);
+		easy.finishLevel();
+	}
+	
+	
 }
