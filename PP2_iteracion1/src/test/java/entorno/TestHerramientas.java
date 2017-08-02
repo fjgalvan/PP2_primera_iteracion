85ee40541c1579ca07791cc6e77b51474564d1c1
fjgalvan <fjgalvanx87@gmail.com>
2017-08-02 19:59:10 -0300

    test de Herramientas

---
+++ PP2_iteracion1/src/test/java/entorno/TestHerramientas.java
@@ -0,0 +1,20 @@
+package entorno;
+
+import javax.sound.sampled.Clip;
+
+import org.junit.Test;
+
+public class TestHerramientas {
+	@Test
+	public void testHerramientas() {
+		//Herramientas h= new Herramientas();
+		
+		Herramientas.radianes(0);
+		Herramientas.grados(0);
+		//Herramientas.play("/sonido/Tanque Disparo.wav");
+		Clip sonido = Herramientas.cargarSonido("Tanque Disparo.wav");
+		sonido.loop(Clip.LOOP_CONTINUOUSLY);
+		Herramientas.play("Tanque Disparo.wav");
+	}
+	
+}
